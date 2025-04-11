b6594a84492540d3f92b0863280074478cdfe833
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-04-11 11:29:49 +0300

    patch points + basic goto impl + write to curInstrOffset, when needed

--- cpp_jcfu/InstrCompiler.hpp
+++ cpp_jcfu/InstrCompiler.hpp
@@ -74,15 +74,26 @@ namespace cpp_jcfu
 		&& !BaseBranched<T>
 		&& !PushConstXed<T>;
 
+	struct PatchPoint
+	{
+		int32_t instrOffset : 31;
+		int32_t is32Bit : 1 = false;
+		uint16_t instrIdx;
+		uint16_t byteOffset;
+	};
+
 	inline std::vector<uint8_t> compileInstrs(
 		size_t& poolSize, ConstPool& consts,
 		const std::vector<Instr>& instrs
 	)
 	{
+		_ASSERT(instrs.size() < UINT16_MAX);
+
 		//TODO: build list of instr offsets
 		//TODO: build list of bytes to patch into relative instr offsets
 		//TODO: solve relative instr sizes
 
+		std::vector<PatchPoint> instrPatchPoints;
 		std::vector<uint16_t> instrOffsets(instrs.size());
 		size_t curInstrOffset = 0;
 
@@ -90,7 +101,7 @@ namespace cpp_jcfu
 		out.reserve(instrs.size() + (instrs.size() >> 3)); // 1.125X scaling
 
 
-		for (size_t i = 0; i < instrs.size(); i++)
+		for (uint16_t i = 0; i < instrs.size(); i++)
 		{
 			const Instr& instr = instrs[i];
 
@@ -110,35 +121,42 @@ namespace cpp_jcfu
 			varcase(const InstrType::I_PUSH_I32_I8) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				out.push_back(var);
+				curInstrOffset++;
 			},
 			varcase(const InstrType::I_PUSH_I32_I16) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				u16w(out, var);
+				curInstrOffset += 2;
 			},
 
 			varcase(const InstrType::I_PUSH_CONST_U8) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				out.push_back(var.poolIdx);
+				curInstrOffset++;
 			},
 			varcase(const PushConstXed auto) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				u16w(out, var.poolIdx);
+				curInstrOffset += 2;
 			},
 
 			varcase(const BaseVarInstred auto) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				out.push_back(var.varIdx);
+				curInstrOffset++;
 			},
 
 			varcase(const InstrType::I_ADD_I32_VAR_U8_CI8) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				out.push_back(var.varIdx);
 				out.push_back(var.val);
+				curInstrOffset += 2;
 			},
 
 			varcase(const BaseBranched16 auto) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				u16w(out, var.jmpOffsetBytes);
+				curInstrOffset += 2;
 			},
 
 			varcase(const InstrType::TABLE_SWITCH&) {
@@ -153,6 +171,7 @@ namespace cpp_jcfu
 			varcase(const BaseRefed auto&) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				constPoolIdxPushW(out, poolSize, consts, *var.ref);
+				curInstrOffset += 2;
 			},
 
 			varcase(const InstrType::PUSH_RUN_INTERFACE&) {
@@ -160,26 +179,32 @@ namespace cpp_jcfu
 				constPoolIdxPushW(out, poolSize, consts, *var.ref);
 				out.push_back(var.argCount);
 				out.push_back(0);
+				curInstrOffset += 4;
 			},
 			varcase(const InstrType::PUSH_RUN_DYN&) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				constPoolIdxPushW(out, poolSize, consts, *var.ref);
 				out.push_back(0);
 				out.push_back(0);
+				curInstrOffset += 4;
 			},
 
 			varcase(const InstrType::PUSH_ARR) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				out.push_back((uint8_t)var.type);
+				curInstrOffset++;
 			},
 			varcase(const InstrType::PUSH_OBJARR_U8&) {
 				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
 				constPoolIdxPushW(out, poolSize, consts, *var.ref);
 				out.push_back(var.dims);
+				curInstrOffset += 3;
 			},
 
 			varcase(const BaseBranched32 auto) {
-				//TODO
+				pushOpCodeByte(out, instrOffsets, curInstrOffset, i, var);
+				u32w(out, var.jmpOffsetBytes);
+				curInstrOffset += 4;
 			},
 
 			// Wide
@@ -212,7 +237,32 @@ namespace cpp_jcfu
 			},
 
 			varcase(const InstrType::GOTO) {
-				//TODO
+				if (var.jmpOffset > INT16_MAX || var.jmpOffset < INT16_MIN)
+				{// Always 32
+					pushOpCodeId(out, instrOffsets, curInstrOffset, i, 
+						InstrId::GOTO32);
+					u32w(out, 0);
+					instrPatchPoints.push_back(PatchPoint{
+						.instrOffset=var.jmpOffset,
+						.is32Bit=true,
+						.instrIdx=i,
+						.byteOffset=(uint16_t)curInstrOffset
+						});
+					curInstrOffset += 4;
+				}
+				else
+				{// Hope for 16
+					pushOpCodeId(out, instrOffsets, curInstrOffset, i,
+						InstrId::GOTO16);
+					u16w(out, 0);
+					instrPatchPoints.push_back(PatchPoint{
+						.instrOffset = var.jmpOffset,
+						.is32Bit = false,
+						.instrIdx = i,
+						.byteOffset = (uint16_t)curInstrOffset
+						});
+					curInstrOffset += 2;
+				}
 			},
 
 			varcase(const BaseBranched auto) {
