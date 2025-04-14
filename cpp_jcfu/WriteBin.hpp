6672f83e71c7a92a6fc7e08bfc814ffab64a80a4
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2025-04-14 18:44:02 +0300

    Write line nums

--- cpp_jcfu/WriteBin.hpp
+++ cpp_jcfu/WriteBin.hpp
@@ -142,8 +142,21 @@ namespace cpp_jcfu
 	inline void codeTagW(std::vector<uint8_t>& out, size_t& poolSize, ConstPool& consts, const CodeTag& itm)
 	{
 		ezmatch(itm)(
+		// https://docs.oracle.com/javase/specs/jvms/se24/html/jvms-4.html#jvms-4.7.12
 		varcase(const CodeTagType::LINE_NUMS&){
-			//TODO
+			pushJutf8IdxW(out, poolSize, consts, "LineNumberTable");
+			std::vector<uint8_t> tagOut;
+
+			_ASSERT(var.size() < UINT16_MAX);
+			u16w(tagOut, (uint16_t)var.size());
+			for (const CodeTagLineNumEntry& e : var)
+			{
+				u16w(tagOut, e.startPc);
+				u16w(tagOut, e.line);
+			}
+			_ASSERT(tagOut.size() < UINT32_MAX);
+			u32w(out, (uint32_t)tagOut.size());
+			out.insert(out.end(), tagOut.begin(), tagOut.end());
 		},
 		varcase(const CodeTagType::LOCALS&){
 			//TODO
